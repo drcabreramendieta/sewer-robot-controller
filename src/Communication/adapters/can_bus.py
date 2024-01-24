7278f3267e016fa3c8495b0d7f6df71b43c9d782
Deiver Jimenez <deiverjimenezsantin@gmail.com>
2024-01-24 09:39:22 -0500

    Improved CAN communication

--- src/Communication/adapters/can_bus.py
+++ src/Communication/adapters/can_bus.py
@@ -11,74 +11,57 @@ class CANRobotLink(RobotLink):
 
     def send(self, wheelsModuleLeft:WheelsModule, wheelsModuleRight:WheelsModule) -> None:
         # Get the speed message to the wheels modules considering the side. There are two ranges of values 
-        # depending on the direction of rotation. The first goes ascending from 0x0000 to 0x7FFE, being 0% 
-        # and 100% respectively. The second goes descending from 0xFFFF to 0x8000, being 0% and 100%, 
-        # respectively.        
-            
-            if wheelsModuleLeft.direction == "F":
-                speed_left = f'{int((wheelsModuleLeft.speed / 100) * 0x7FFE):016b}'
-            elif wheelsModuleLeft.direction == "R":
-                speed_left = f'{int((wheelsModuleLeft.speed / 100) * 0x7FFE):016b}'
-            elif wheelsModuleLeft.direction == "L":
-                speed_left = f'{int((100 - wheelsModuleLeft.speed) / 100 * (0xFFFF - 0x8000)) + 0x8000:016b}'
-            elif wheelsModuleLeft.direction == "B":
-                speed_left = f'{int((100 - wheelsModuleLeft.speed) / 100 * (0xFFFF - 0x8000)) + 0x8000:016b}'
-            elif wheelsModuleLeft.direction == "S":
-                speed_left = f'{0:016b}'
-            
-            if wheelsModuleRight.direction == "F":
-                speed_right = f'{int((100 - wheelsModuleLeft.speed) / 100 * (0xFFFF - 0x8000)) + 0x8000:016b}'
-            elif wheelsModuleRight.direction == "R":
-                speed_right = f'{int((wheelsModuleRight.speed / 100) * 0x7FFE):016b}'
-            elif wheelsModuleRight.direction == "L":
-                speed_right = f'{int((100 - wheelsModuleLeft.speed) / 100 * (0xFFFF - 0x8000)) + 0x8000:016b}'
-            elif wheelsModuleRight.direction == "B":
-                speed_right = f'{int((wheelsModuleRight.speed / 100) * 0x7FFE):016b}'
-            elif wheelsModuleRight.direction == "S":
-                speed_right = f'{0:016b}' 
+        # depending on the direction of rotation. The speed values go from -2048 to 2047, with 0 being the change
+        # side of rotation.
+        speed_left = 0
+        speed_right = 0
 
-            speed_left_data0 = int(speed_left[:8],2)
-            speed_left_data1 = int(speed_left[8:],2)
-            
-            speed_right_data0 = int(speed_right[:8],2)
-            speed_right_data1 = int(speed_right[8:],2)
-            
+        if wheelsModuleLeft.direction == "F":
+            speed_left = int(-2048 * (wheelsModuleLeft.speed / 100))
+        elif wheelsModuleLeft.direction == "B":
+            speed_left = int(2047 * (wheelsModuleLeft.speed / 100))
+        elif wheelsModuleLeft.direction == "S":
+            speed_left = 0
 
-            m1 = [speed_left_data0, speed_left_data1, speed_left_data0, speed_left_data1, \
-                  speed_left_data0, speed_left_data1, speed_right_data0, speed_right_data1]
-            m2 = [speed_right_data0, speed_right_data1, speed_right_data0, speed_right_data1,\
-                  0, 0, 0, 0]
-            
+        if wheelsModuleRight.direction == "F":
+            speed_right = int(2047 * (wheelsModuleRight.speed / 100))
+        elif wheelsModuleRight.direction == "B":
+            speed_right = int(-2048 * (wheelsModuleRight.speed / 100))
+        elif wheelsModuleRight.direction == "S":
+            speed_right = 0
 
-        #Configure CAN protocol communication 
-            try:
-                bus = can.interface.Bus(device_id=0, interface='socketcan')
-                message1 = can.Message(arbitration_id=0x0202, data=m1)
-                message2 = can.Message(arbitration_id=0x0203, data=m2)
-                
-                bus.send(message1)
-                bus.send(message2)
+        speed_left = format((speed_left + (1 << 16)) % (1 << 16), '0{}b'.format(16))
+        speed_left_data0 = int(speed_left[:8], 2)
+        speed_left_data1 = int(speed_left[8:], 2)
 
-                print('Mensaje enviado')
-                print(message1.data)
-                print(message2.data)
+        speed_right = format((speed_right + (1 << 16)) % (1 << 16), '0{}b'.format(16))
+        speed_right_data0 = int(speed_right[:8], 2)
+        speed_right_data1 = int(speed_right[8:], 2)
 
-            except can.CanError as e:
-                print(f"Error CAN: {e}")
-        
-            finally:
-                if 'bus' in locals():
-                    bus.shutdown()
-            
+        m1 = [speed_left_data0, speed_left_data1, speed_left_data0, speed_left_data1, speed_left_data0,
+              speed_left_data1, speed_right_data0, speed_right_data1]
+        m2 = [speed_right_data0, speed_right_data1, speed_right_data0, speed_right_data1, 0, 0, 0, 0]
 
-            
-            
-                
-            
+        print(m1)
+        # Configure CAN protocol communication
+        try:
+            bus = can.interface.Bus(device_id=0, interface='pcan', bitrate=1000000)
+            message1 = can.Message(arbitration_id=0x0202, data=m1, is_extended_id=False)
+            message2 = can.Message(arbitration_id=0x0203, data=m2, is_extended_id=False)
+            print(message1)
+            print(message2)
+            bus.send(message1)
+            bus.send(message2)
 
-            
-            
+            print('Mensaje enviado')
 
+        except can.CanError as e:
+            print(f"Error CAN: {e}")
+
+        finally:
+            if 'bus' in locals():
+                bus.shutdown()
+    
     def callback_setup(self, callback:Callable[[Telemetry], None]) -> None:
         self.callback = callback
 
