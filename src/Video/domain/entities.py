0382ee1b6f514281513184ecdd84a0710f49bf33
Diego <drcabreramendieta@gmail.com>
2024-04-04 18:51:21 -0500

    Base of dvr

--- src/Video/domain/entities.py
+++ src/Video/domain/entities.py
@@ -1,5 +1,31 @@
 from dataclasses import dataclass
+from enum import IntEnum
 
 @dataclass
 class VideoMessage:
     frame: list
+
+class DvrOrder(IntEnum):
+    START_RECORDING = 1
+    STOP_RECORDING = 2
+    TAKE_PHOTO = 3
+    GET_VIDEO = 4
+    GET_PHOTO = 5
+
+@dataclass
+class Session:
+    name: str
+    captures: list
+    records: list
+    recording: bool
+
+@dataclass
+class ImageInfo:
+    path: str
+    capture_date_time: str
+
+@dataclass
+class RecordInfo:
+    path: str
+    record_date_time: str
+    duration: int
---
+++ src/Video/domain/use_cases/control_session.py
@@ -0,0 +1,20 @@
+from Video.ports.dvr_link import DvrLink
+from Video.domain.entities import Session
+from Video.domain.entities import DvrOrder
+
+class ControlSession:
+    def __init__(self, dvr_link:DvrLink) -> None:
+        self.dvr_link = dvr_link
+        self.session = None
+
+    def create_session(self, name:str, photos:list|None, videos:list|None) -> Session:
+        self.session = Session(name=name, photos=photos, videos=videos)
+
+    def run(self, order:DvrOrder):
+        if self.session:
+            if order == DvrOrder.TAKE_PHOTO:
+                self.session.captures.append(self.dvr_link.take_image())
+            elif order == DvrOrder.START_RECORDING:
+                self.session.recording = self.dvr_link.start_recording()
+            elif order == DvrOrder.STOP_RECORDING and self.session.recording:
+                self.session.records.append(self.dvr_link.stop_recording())
